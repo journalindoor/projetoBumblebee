afdddc9304877d07004616d18e04e7957429614b
RSMJr <journal.indoor@gmail.com>
2019-03-03 06:52:10 -0300

    Adicionando movimento para os lados

--- js/comandos.js
+++ js/comandos.js
@@ -1,23 +1,39 @@
-document.onkeydown = qualSeta;
+
+
+var palco = document.getElementById('palco');
+var junin = document.getElementById('junin');
+var juninEsq = 0;
+var juninBai = 0;
 
 
 function qualSeta(e) {
 
-	var junin = document.getElementById('boneco-jr');
-
-    e = e || window.event;
-
     if (e.keyCode == '38') {
-        console.log('up arrow');
+        console.log('seta cima');
     }
     else if (e.keyCode == '40') {
-        console.log('down arrow');
+        console.log('seta baixo');
     }
     else if (e.keyCode == '37') {
-       console.log('left arrow');
+       console.log('seta esquerda');
+       juninEsq -= 2;
+       junin.style.left = juninEsq + 'px';
+       if(juninEsq <= 0){
+       		juninEsq += 2;
+       }
     }
     else if (e.keyCode == '39') {
-       console.log('right arrow');
+       console.log('seta direita');
+       juninEsq += 2;
+       junin.style.left = juninEsq + 'px';
     }
+
+}
+
+document.onkeydown = qualSeta;
+
+function pula(){
+
+
 
 }
