13631827715ceb58944b8f794cd333f0a33a3677
RSMJr <journal.indoor@gmail.com>
2019-03-06 22:22:51 -0300

    Mudanca no projeto, remocao do pulo e adicionando um chao

--- js/comandos.js
+++ js/comandos.js
@@ -3,17 +3,19 @@
 var palco = document.getElementById('palco');
 var junin = document.getElementById('junin');
 var juninEsq = 0;
-var velocidade = 5;
+var juninBai = 70;
+var velocidade = 10;
 
 
 function qualSeta(e) {
 
-  if (e.keyCode == '38' || e.keyCode == '32') {
-    console.log('seta cima ou barra espaco');
-    pulando();
+  if (e.keyCode == '38') {
+    console.log('seta cima');
+    andaPraCima();
   }
   else if (e.keyCode == '40') {
     console.log('seta baixo');
+    andaPraBaixo();
   }
   else if (e.keyCode == '37') {
    console.log('seta esquerda');
@@ -29,24 +31,31 @@ function qualSeta(e) {
 
 document.onkeydown = qualSeta;
 
+function andaPraCima(){
+  if(juninBai >= 230){
+   juninBai -= velocidade;
+ }
+ juninBai += velocidade;
+ junin.style.bottom = juninBai + 'px';  
+}
+
+function andaPraBaixo(){
+  if(juninBai <= 70){
+   juninBai += velocidade;
+ }
+  juninBai -= velocidade;
+  junin.style.bottom = juninBai + 'px';
+}
+
 function andaPraEsquerda(){
-  juninEsq -= velocidade;
-  junin.style.left = juninEsq + 'px';
   if(juninEsq <= 0){
    juninEsq += velocidade;
  }
+  juninEsq -= velocidade;
+  junin.style.left = juninEsq + 'px';
 }
 
 function andaPraDireita(){
-   juninEsq += velocidade;
-   junin.style.left = juninEsq + 'px';  
-}
-
-function pulando(){
-  junin.classList.add('pulando');
-  junin.addEventListener("animationend", fimDoPulo);
-}
-
-function fimDoPulo(){
-  this.classList.remove('pulando');
+ juninEsq += velocidade;
+ junin.style.left = juninEsq + 'px';  
 }
