a7b63b1e22e2b10fd44e8d8ceb2ccc7ed332fa61
RSMJr <journal.indoor@gmail.com>
2019-05-26 22:49:54 -0300

    Funções para ligar a moto e o farol

--- js/bumblee.js
+++ js/bumblee.js
@@ -1,3 +1,24 @@
 window.onload = function () {
     console.log('Onload disparado');
+
+    partida();
+    ligaFarol();
+}
+
+function partida() {
+    var motor = document.getElementsByClassName('motor');
+    motor[0].addEventListener('click', function() {
+        var movel = document.getElementsByClassName('movel');
+        for (i = 0; i < movel.length; i++) {
+            console.log('Peça: ', movel[i].classList.toggle('balanca'));
+        }
+    });
+}
+
+
+function ligaFarol() {
+    var farol = document.getElementsByClassName('farol');
+    farol[0].addEventListener('click', function() {
+        this.classList.toggle('aceso');
+    });
 }
